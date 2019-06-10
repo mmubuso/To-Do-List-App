c2b9a97e88d8aded5e49568f53fdaada0a09a3d3
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-06-10 19:17:08 -0400

    Added a strike through optoon to show completion

--- Starter-Code/src/app.js
+++ Starter-Code/src/app.js
@@ -13,19 +13,21 @@ $(document).ready(function () {
     submit.on('click',function(){
         let toDoItem = $('input').val()
         if((toDoItem.length) > 0){
-            $('ul').prepend(`<li class="list-item">${toDoItem}<i class="complete fas fa-plus-square"></i><i class="delete fas fa-trash-alt"></i></li>`)
+            $('ul').prepend(`<li class="list-item">${toDoItem}<i class="fas fa-plus-square"></i><i class="delete fas fa-trash-alt"></i></li>`)
         }
 
     })  
 
-    //Remove the trash can
+    //Remove the to do it tem
     $('ul').on('click',function(event){
         console.log(event)
         if(event.target.className === "delete fas fa-trash-alt"){
             event.target.parentNode.remove()
             console.log(event)
+        }else if(event.target.className === "fas fa-plus-square"){
+            event.target.parentNode.classList.toggle('complete')
         }else{
-            return
+
         }
      })
 
