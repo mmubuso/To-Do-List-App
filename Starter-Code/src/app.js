bcdedf3eb42b2def694892b5decc6f586fdac001
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-06-10 21:55:44 -0400

    Added expression for clearing input once to do list task has been added.

--- Starter-Code/src/app.js
+++ Starter-Code/src/app.js
@@ -11,15 +11,29 @@ $(document).ready(function () {
     // Event Listeners
     //listen for click on submit button
     submit.on('click',function(){
+        event.preventDefault()
+
         let toDoItem = $('input').val()
         if((toDoItem.length) > 0){
             $('ul').prepend(`<li class="list-item">${toDoItem}<i class="fas fa-plus-square"></i><i class="delete fas fa-trash-alt"></i></li>`)
+            $('input').val('')
         }
+        
+    })  
 
+    // If enter key is pressed down
+    $(document).eq(0).on( "keypress",function(){
+        let toDoItem = $('input').val()
+        if((toDoItem.length) > 0 && event.keyCode == 13){
+            event.preventDefault()
+            $('ul').prepend(`<li class="list-item">${toDoItem}<i class="fas fa-plus-square"></i><i class="delete fas fa-trash-alt"></i></li>`)
+            $('input').val('')
+        }
+        
     })  
 
     //Remove the to do it tem
-    $('ul').on('click',function(event){
+    $('ul').on('click',function(){
         console.log(event)
         if(event.target.className === "delete fas fa-trash-alt"){
             event.target.parentNode.remove()
